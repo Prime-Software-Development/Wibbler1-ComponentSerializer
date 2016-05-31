ec78d4e82adc58a475b76fb0b67da97a090411ad
nas <nas@e266bc92-ee96-4d19-87e6-24e55d6e7a6d>
2016-05-31 11:30:31 +0000

    added callbacks

---
+++ tags/0.0.7/Mapping/Loader/PhpLoader.php
@@ -0,0 +1,70 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace TrunkSoftware\Component\Serializer\Mapping\Loader;
+
+use Symfony\Component\Serializer\Mapping\Loader\LoaderInterface;
+
+use Symfony\Component\Config\Util\XmlUtils;
+use Symfony\Component\Serializer\Exception\MappingException;
+#use Symfony\Component\Serializer\Mapping\AttributeMetadata;
+use Symfony\Component\Serializer\Mapping\ClassMetadataInterface;
+
+use TrunkSoftware\Component\Serializer\Mapping\AttributeMetadata;
+
+/**
+ * Loads XML mapping files.
+ *
+ * @author Kévin Dunglas <dunglas@gmail.com>
+ */
+class PhpLoader implements LoaderInterface
+{
+	public function loadClassMetadata(ClassMetadataInterface $classMetadata)
+	{
+		// only custom propel attribute metadata loader
+		if( ! $classMetadata->getReflectionClass()->hasMethod('getAttributesMetadata') )
+			return true;
+
+		$attributesMetadata = $classMetadata->getAttributesMetadata();
+
+		$attributes = $classMetadata->getReflectionClass()->getMethod('getAttributesMetadata')->invoke( null );
+		$callbacks = $classMetadata->getReflectionClass()->hasMethod('getAttributesCallbacks') ? $classMetadata->getReflectionClass()->getMethod('getAttributesCallbacks')->invoke( null ) : NULL;
+
+		foreach ($attributes as $attribute) {
+			$attributeName = (string) $attribute['name'];
+
+			if (isset($attributesMetadata[$attributeName])) {
+				$attributeMetadata = $attributesMetadata[$attributeName];
+			} else {
+				$attributeMetadata = new AttributeMetadata($attributeName);
+				$classMetadata->addAttributeMetadata($attributeMetadata);
+			}
+
+			foreach ($attribute['group'] as $group) {
+				$attributeMetadata->addGroup((string) $group);
+			}
+
+			if(isset($attribute['convert_name'])) {
+				$attributeMetadata->setConvertedName( $attribute['convert_name'] );
+			}
+
+			if(isset($attribute['flatten'])) {
+				$attributeMetadata->setFlatten( $attribute['flatten'] );
+			}
+
+			if( $callbacks && isset($callbacks[ $attributeName ]) ){
+				$attributeMetadata->setCallback( $callbacks[ $attributeName ] );
+			}
+		}
+
+		return true;
+	}
+}
---
+++ tags/0.0.7/NameConverter/PropelNameConverter.php
@@ -0,0 +1,44 @@
+<?php
+namespace TrunkSoftware\Component\Serializer\NameConverter;
+
+use Propel\Runtime\ActiveRecord\ActiveRecordInterface;
+use Symfony\Component\Serializer\Mapping\AttributeMetadata;
+use Symfony\Component\Serializer\Mapping\AttributeMetadataInterface;
+use Symfony\Component\Serializer\NameConverter\NameConverterInterface;
+
+class PropelNameConverter implements NameConverterInterface
+{
+	private $attributeMetadata;
+	private $context;
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function normalize($attributesMetadata)
+	{
+		if( $attributesMetadata->getConvertName() )
+			return $attributesMetadata->getConvertName();
+
+		return $attributesMetadata->getName();
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function denormalize($propertyName)
+	{
+		/*$camelCasedName = preg_replace_callback('/(^|_|\.)+(.)/', function ($match) {
+			return ('.' === $match[1] ? '_' : '').strtoupper($match[2]);
+		}, $propertyName);
+
+		if ($this->lowerCamelCase) {
+			$camelCasedName = lcfirst($camelCasedName);
+		}
+
+		if (null === $this->attributes || in_array($camelCasedName, $this->attributes)) {
+			return $this->lowerCamelCase ? lcfirst($camelCasedName) : $camelCasedName;
+		}*/
+
+		return $propertyName;
+	}
+}
