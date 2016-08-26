a4dc9a093efcedf003a93264f869a95924e1cb10
nas <nas@e266bc92-ee96-4d19-87e6-24e55d6e7a6d>
2016-08-26 10:34:11 +0000

    handle attribute as string bugfix

---
+++ tags/0.0.13/Mapping/AttributeMetadata.php
@@ -0,0 +1,32 @@
+<?php
+namespace TrunkSoftware\Component\Serializer\Mapping;
+
+use Symfony\Component\Serializer\Mapping\AttributeMetadata as BaseAttributeMetadata;
+use Symfony\Component\Serializer\Mapping\AttributeMetadataInterface;
+
+class AttributeMetadata extends BaseAttributeMetadata {
+
+	public $callback;
+
+	public $flatten;
+
+	public $converted_name;
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function __construct($name)
+	{
+		$this->name = $name;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function merge(AttributeMetadataInterface $attributeMetadata)
+	{
+		foreach ($attributeMetadata->getGroups() as $group) {
+			$this->addGroup($group);
+		}
+	}
+}
